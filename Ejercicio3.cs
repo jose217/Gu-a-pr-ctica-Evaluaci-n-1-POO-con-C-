6542a50acfc0f4fdde984fb6376495b281e3121d
JSDVS <joseramp99@hotmail.com>
2024-02-16 16:45:44 -0600

    ej3y1

---
+++ Ejercicio3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio3
+{
+    internal class Ejercicio3
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            string texto = "El número ingresado es ";
+            Console.WriteLine("Ingrese un numero entero: ");
+            n = int.Parse(Console.ReadLine());
+            texto = n>0 ? texto + "positivo" : (n<0 ? texto + "negativo": texto + "cero");
+            Console.WriteLine(texto);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-Guia-practica-Evaluacion-1-POO-con-C#
-	
-INTEGRANTES:
-25 4209 2022 - Hernandez Beltran, Tommy Stanley
-25 0977 2021 - Ramos Pérez, José Enock
+# Guia-practica-Evaluacion-1-POO-con-C#
+#	
+# INTEGRANTES:
+# 25 4209 2022 - Hernandez Beltran, Tommy Stanley
+# 25 0269 2023 - Hernández García, Macdonio Bladimir
+# 25 0977 2021 - Ramos Pérez, José Enock
